c29b9773a0962167d08bf530243d9238832faf17
DarcorTweaks <darwingcorreaps4@gmail.com>
2026-03-06 00:27:33 -0400

    Update install_hesuvi.ps1

--- tools/install_hesuvi.ps1
+++ tools/install_hesuvi.ps1
@@ -1,16 +1,12 @@
 Write-Host ""
 Write-Host "Installing HeSuVi..." -ForegroundColor Cyan
 
-$installer = "$PSScriptRoot\..\installers\HeSuVi_2.0.0.1.exe"
+$temp = "$env:TEMP\hesuvi.exe"
 
-if(Test-Path $installer){
+$url = "https://sourceforge.net/projects/hesuvi/files/latest/download"
 
-Start-Process $installer -Verb RunAs -Wait
+Invoke-WebRequest $url -OutFile $temp
+
+Start-Process $temp -Wait
 
 Write-Host "HeSuVi installation finished." -ForegroundColor Green
-
-}else{
-
-Write-Host "HeSuVi installer not found." -ForegroundColor Red
-
-}

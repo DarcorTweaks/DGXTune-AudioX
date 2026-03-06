50e7d9601b949bf3dba7be9631f51003641472bf
DarcorTweaks <darwingcorreaps4@gmail.com>
2026-03-06 00:06:50 -0400

    Update install_voicemeeter.ps1

--- tools/install_voicemeeter.ps1
+++ tools/install_voicemeeter.ps1
@@ -5,15 +5,15 @@ $temp = "$env:TEMP\voicemeeter.exe"
 
 $url = "https://download.vb-audio.com/Download_CABLE/VoicemeeterSetup.exe"
 
-try{
+try {
 
 Start-BitsTransfer -Source $url -Destination $temp
 
 Start-Process $temp -Wait
 
-Write-Host "Voicemeeter installed." -ForegroundColor Green
+Write-Host "Voicemeeter installed successfully." -ForegroundColor Green
 
-}catch{
+} catch {
 
 Write-Host "Failed to download Voicemeeter." -ForegroundColor Red
 
